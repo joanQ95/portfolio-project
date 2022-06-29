fb9bd8364acade6e3505cdb888d037e2d0ff3c26
Rumpels95 <joan.quispec@gmail.com>
2022-06-28 19:43:38 -0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <h2>Portfolio Project</h2>
+    </>
   );
 }
 
